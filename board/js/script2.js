9dfbbf17265b73433dc43c045800bf46e47d6910
Kimjw111 <whale960105@gmail.com>
2023-06-10 20:00:07 +0900

    게시판

--- board/js/script2.js
+++ board/js/script2.js
@@ -1,12 +1,25 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    const content=document.querySelector('.content')
-    const div1=document.createElement('div')
-    const div2=document.createElement('div')
-    const div3=document.createElement('div')
-    const div4=document.createElement('div')
-    const div5=document.createElement('div')
-    content.append(div1,div2,div3,div4,div5)
-    let ct=1
-div1.innerHTML=ct++
-div2.innerHTML=JSON.parse(localStorage.getItem('input'))
+document.addEventListener('DOMContentLoaded', () => {
+    const content = document.querySelector('.content')
+    let a = 1
+    let arr1 = JSON.parse(localStorage.getItem('input'))
+
+
+    for (let i = 0; i < arr1.length; i++) {
+        const boardList = document.createElement('div')
+        const count = document.createElement('div')
+        const title = document.createElement('div')
+        const name = document.createElement('div')
+        const ct = document.createElement('div')
+        const date = document.createElement('div')
+        boardList.setAttribute('class', 'boardList')
+        boardList.setAttribute('id', 'boardList' + i)
+        title.setAttribute('class', 'title')
+        count.innerHTML = a + i
+        title.innerHTML = arr1[i].제목
+        name.innerHTML = arr1[i].작성자
+        date.innerHTML = arr1[i].작성일자
+        content.appendChild(boardList)
+        boardList.append(count, title, name, ct, date)
+    }
+
 })
--- board/js/script3.js
+++ board/js/script3.js
@@ -1,25 +1,30 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    const submit=document.getElementById('commit')
-    let arr=[]
-        submit.addEventListener('click',()=>{
-        const title=document.getElementById('title')
-        const content=document.getElementById('content')
-        const name=document.getElementById('name')
-        if (title.value == "") {
-            alert('제목을 입력해주세요.');
-            title.focus();
-          } else if (content.value == "") {
-            alert('내용을 입력해주세요.');
-            content.focus()
-          } else if (name.value == "") {
-            alert('작성자를 입력해주세요.');
-            name.focus();
-          }else{
-             arr.push({제목:title.value,내용:content.value,작성자:name.value})
-            localStorage.setItem('input',JSON.stringify(arr))
-            title.value='';
-            content.value='';
-            name.value='' ;
-        } 
-    })
+document.addEventListener('DOMContentLoaded', () => {
+
+  const submit = document.getElementById('commit')
+  let arr = JSON.parse(localStorage.getItem('input')) ?? []
+  submit.addEventListener('click', () => {
+    let today = new Date();
+    let year = today.getFullYear(); // 년도
+    let month = today.getMonth() + 1;  // 월
+    let date = today.getDate();  // 날짜
+    const title = document.getElementById('title')
+    const content = document.getElementById('content')
+    const name = document.getElementById('name')
+    if (title.value == "") {
+      alert('제목을 입력해주세요.');
+      title.focus();
+    } else if (content.value == "") {
+      alert('내용을 입력해주세요.');
+      content.focus()
+    } else if (name.value == "") {
+      alert('작성자를 입력해주세요.');
+      name.focus();
+    } else {
+      arr.push({ 제목: title.value, 내용: content.value, 작성자: name.value, 작성일자: year + '/' + month + '/' + date })
+      localStorage.setItem('input', JSON.stringify(arr))
+      title.value = '';
+      content.value = '';
+      name.value = '';
+    }
+  })
 })
